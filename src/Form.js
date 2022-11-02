7cfe7e1445cce2dc573ca2f004939adcf31ebb3f
Ali Nasir <alee.nasir254@gmail.com>
2022-11-02 10:03:37 +0500

    Ui completed

---
+++ src/Form.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Form.css";
+
+export default function Form() {
+  return (
+    <div className="container">
+      <form>
+        <h1>Login Form</h1>
+        <div className="ui divider"></div>
+        <div className="ui form">
+          <div className="field">
+            <label>Username</label>
+            <input type="text" name="username" placeholder="Username" />
+          </div>
+          <div className="field">
+            <label>Email</label>
+            <input type="email" name="email" placeholder="Email" />
+          </div>
+          <div className="field">
+            <label>Password</label>
+            <input type="password" name="password" placeholder="Password" />
+          </div>
+          <br />
+          <button className="fluid ui button blue ">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+}
